22570e278835c1b9aed4b93323931c076516b50b
Austin Bullard <abullard@basecampcodingacademy.org>
2017-07-26 16:16:37 -0500

    fixed flaws within the program and made it look cleaner

--- shell.py
+++ shell.py
@@ -1,31 +1,45 @@
 import core
 import disk
 from core import car_price
+import sys, time
+
+typing_speed = 13
+def slow_type(t):
+    for l in t:
+        sys.stdout.write(l)
+        sys.stdout.flush()
+        time.sleep(typing_speed / 970.0)
+    return input()
+
+
 
 
 # print('Hello World')
 
 def main():
-    msg = '''Welcome to L\'s Garage!! Here is what we have to offer!!
-    \t1. Bugatti Veyron.$40000\n
-    \t2. R.R Drophead Phantom Coupe.$3500\n
-    \t3. Lambo Murcielago.$2250\n
-    \t4. Ferrari F450.$1675\n
-    \t5. Lambo Gallardo.$1600\n
+    msg = '''🚗\nWelcome to L\'s Garage 🕴 🕴 🖐\nHere is what we have to offer ‼\n🏎
+    ╔                                         ╗
+    ├What would you like to drive away today⁈ ┤\n
+    ╏\t1. Bugatti Veyron╌💲40000             ╏\n            
+    ╏\t2. R.R Drophead Phantom Coupe╌💲3500  ╏\n 
+    ╏\t3. Lambo Murcielago╌💲2250            ╏\n           
+    ╏\t4. Ferrari F450╌💲1675                ╏\n
+    ╏\t5. Lambo Gallardo╌💲1600              ╏\n
+    ╚                                         ╝
     '''
     while True:
-        car = input(msg)
+        car = slow_type(msg)
         if car == '1' or car == '2' or car == '3' or car == '4' or car == '5':
             break
         else:
-            print('Sorry, Invalid choice!')
-    days = input('How many days would you like to rent this car?\n')
-    print('Your total will be ${:.2F}'.format(core.car_price(car, days)))
+            print('🚫 Sorry, Invalid choice🕴 🚫')
+    days = slow_type('« ℋ ow many days would you like to rent this car⁈ »\n')
+    print('Your total will be 💲{:.2F}'.format(core.car_price(car, days)))
 
     car_type = disk.keep_history(car, days)
     disk.take_out(car, days)
-    print('Thank you!\n!!!Warning!!!\nWe are not responsible for any speeding tickets!!!\nAny damage to the vehicle with be added to the bill!')
-    
+    print('Thank you!\n')
+    print("⚠"'\nWe are not responsible for any tickets ‼\n'"⚠")
+
 if __name__ == '__main__':
     main()
-
--- test_core.py
+++ test_core.py
@@ -14,9 +14,9 @@ def test_car_type():
     
 
 def test_car_price():
-    assert core.car_price('1', 2) == 40000
-    assert core.car_price('2', 4) == 3500
-    assert core.car_price('3', 6) == 2250
-    assert core.car_price('4', 8) == 1675
-    assert core.car_price('5', 10) == 1600
+    assert core.car_price('1', 2) == 85600.0
+    assert core.car_price('2', 4) == 14980.0
+    assert core.car_price('3', 6) == 14445.0
+    assert core.car_price('4', 8) == 14338.0
+    assert core.car_price('5', 10) == 17120.0
     
