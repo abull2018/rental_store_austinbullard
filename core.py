296cd9d222d0d00b32aea2da920d467069eed315
Austin Bullard <abullard@basecampcodingacademy.org>
2017-07-27 09:46:31 -0500

    Made everything a comment so I could make more options

--- core.py
+++ core.py
@@ -1,49 +1,49 @@
-def car_type(car):
-    '''str -> str'''
-    message = ''
-    if car == '1' or car.lower() == 'one':
-        car_type = 'Bugatti Veyron'
-    elif car == '2' or car.lower() == 'two':
-        car_type = 'R.R Drophead Phantom Coupe'
-    elif car == '3' or car.lower() == 'three':
-        car_type = 'Lambo Murcielago'
-    elif car == '4' or car.lower() == 'four':
-        car_type = 'Ferrari F450'
-    elif car == '5' or car.lower() == 'five':
-        car_type = 'Lambo Gallardo'
-    return car_type 
+# def car_type(car):
+#     '''str -> str'''
+#     message = ''
+#     if car == '1' or car.lower() == 'one':
+#         car_type = 'Bugatti Veyron'
+#     elif car == '2' or car.lower() == 'two':
+#         car_type = 'R.R Drophead Phantom Coupe'
+#     elif car == '3' or car.lower() == 'three':
+#         car_type = 'Lambo Murcielago'
+#     elif car == '4' or car.lower() == 'four':
+#         car_type = 'Ferrari F450'
+#     elif car == '5' or car.lower() == 'five':
+#         car_type = 'Lambo Gallardo'
+#     return car_type 
 
-def car_price(car, days):
-    '''(str) -> float'''
-    if car == '1':
-        price = float(days) * 40000
-        sales_tax = 0.07
-        tax = price * sales_tax
-        total_price = price + tax
-        return total_price
-    elif car == '2':
-        price = float(days) * 3500
-        sales_tax = 0.07
-        tax = price * sales_tax
-        total_price = price + tax
-        return total_price
-    elif car == '3':
-        price = float(days) * 2250
-        sales_tax = 0.07
-        tax = price * sales_tax
-        total_price = price + tax
-        return total_price 
-    elif car == '4':
-        price = float(days) * 1675
-        sales_tax = 0.07
-        tax = price * sales_tax
-        total_price = price + tax
-        return total_price
-    elif car == '5':
-        price = float(days) * 1600
-        sales_tax = 0.07
-        tax = price * sales_tax
-        total_price = price + tax
-        return total_price 
+# def car_price(car, days):
+#     '''(str) -> float'''
+#     if car == '1':
+#         price = float(days) * 40000
+#         sales_tax = 0.07
+#         tax = price * sales_tax
+#         total_price = price + tax
+#         return total_price
+#     elif car == '2':
+#         price = float(days) * 3500
+#         sales_tax = 0.07
+#         tax = price * sales_tax
+#         total_price = price + tax
+#         return total_price
+#     elif car == '3':
+#         price = float(days) * 2250
+#         sales_tax = 0.07
+#         tax = price * sales_tax
+#         total_price = price + tax
+#         return total_price 
+#     elif car == '4':
+#         price = float(days) * 1675
+#         sales_tax = 0.07
+#         tax = price * sales_tax
+#         total_price = price + tax
+#         return total_price
+#     elif car == '5':
+#         price = float(days) * 1600
+#         sales_tax = 0.07
+#         tax = price * sales_tax
+#         total_price = price + tax
+#         return total_price 
 
 
