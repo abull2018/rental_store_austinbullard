58af38c23f6636dbb3e35588aa4c1c89a5fd2cab
Austin Bullard <abullard@basecampcodingacademy.org>
2017-07-27 15:25:28 -0500

    working on take_out function

--- disk.py
+++ disk.py
@@ -30,28 +30,32 @@ def write_log():
     return cap
 
 def inventory():
-    cap = []
+    left = []
     with open('inventory.txt', 'r') as file:
         file.readline()
         lines = file.readlines()
     for line in lines:
         split_string = line.strip().split(', ')
-        cap.append([split_string[0], float(split_string[1]), float(split_string[2])])
-    return cap
+        left.append([split_string[0], float(split_string[1]), float(split_string[2])])
+    return left
 
-def take_out(car_type, days):
-    str_l = ['car, amount, price']
+def take_out(car_type, amount):
+    str_l = ['Bugatti Veyron, 10, 40000',
+    'R.R Drophead Phantom Coupe, 10, 3500', 
+    'Lambo Murcielago, 10, 2250',
+    'Ferrari F450, 10, 1675',
+    'Lambo Gallardo, 10, 1600']
     left = inventory()
     for item in left:
         if item[0] == car_type:
             if float(amount) > item[1]:
                 print('We rented the last one out. Sorry for the inconvenience')
             else:
-                item[1] = float(item[1]) - float(amount)
+                item[1] = float(item[1]) - float(car)
             item[1] = str(item[1])
             item[2] = str(item[2])
-            str_l = '\n'.join(str_l)
-            left = '\n{}, {}, {}'.format(car, amount, price)
-            with open('inventory.txt', 'w') as file:
-                 file.write(message)
+            str_l.append(', '.join(item))
+    message = '\n'.join(str_l)
+    with open('inventory.txt', 'w') as file:
+            file.write(message)
 
